80375a7c504811d63b015f1fd3547b2ddd16d759
charlesagyemang <micnkru@gmail.com>
2017-09-05 14:12:14 +0000

    Fitst Lesson Initial Commit

---
+++ lesson_one/asgn_one.py
@@ -0,0 +1,7 @@
+'''
+	Find First Assignment in ASSIGNMENTONE.md its in this 
+	folder and come back here and and answer the questions 
+	yh thats how simple this will get.
+'''
+
+#Present Your Answers Below
