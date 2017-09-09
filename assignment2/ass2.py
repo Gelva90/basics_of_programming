5f0a11a2dce52bcb63704d94998537c5e5f719ee
sheidynjiiri <sheidynjiiri@gmail.com>
2017-09-09 16:57:30 +0000

    second assignment

---
+++ assignment2/ass2.py
@@ -0,0 +1,6 @@
+def doubler(number):
+	return number * 2
+
+#test cases 2 = 4 and  3 = 6
+print(doubler(2))
+print(doubler(3))
---
+++ assignment2/ass3.py
@@ -0,0 +1,7 @@
+def age_checker(age):
+	if age <= 20:
+		return ("young")
+	else:
+		return ("old")
+
+print(age_checker(27))
